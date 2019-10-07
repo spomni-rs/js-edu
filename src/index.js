a555e0c283851515448bf0afcff8098b2a110330
spomni <mikhail.snitko@gmail.com>
2019-10-07 14:42:15 +0300

    feat: Find solutions.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+ 
+const javascriptHours = 800;
+const programmingHours = 500;
+ 
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {family: 4}
+  ) {
+  
+  let speed = config[focus];
+  let hours = javascriptHours;
+  
+  if (!knowsProgramming){
+    hours += programmingHours;
+  }
+  
+  return Math.ceil(hours / speed);
+};
   
